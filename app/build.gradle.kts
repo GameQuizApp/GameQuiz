753339aef9cb924394f5fc604370d86a62e90ceb
Alejandro <alejandrord3006@gmail.com>
2023-12-20 13:58:09 +0100

    Build Gradle fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,14 +31,14 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     //para el swipe
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    implementation("com.airbnb.android:lottie:4.0.0")
+    implementation("com.airbnb.android:lottie:6.1.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
     //para el gif
