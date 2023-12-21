de72d53c83603f8ffe2322a0f525e0eee2b563c1
Alejandro <alejandrord3006@gmail.com>
2023-12-21 13:01:27 +0100

    Arreglado el Gradle

--- app/src/main/java/com/example/gamequiz/Splash.java
+++ app/src/main/java/com/example/gamequiz/Splash.java
@@ -12,7 +12,7 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        //LLAMADA AL MÉTODO
+        // LLAMADA AL MÉTODO
         openApp();
         
     }
