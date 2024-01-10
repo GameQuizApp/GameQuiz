5a08ae7033d68358412974beab70493600c8f789
martinguijarro <martin.gg222@gmail.com>
2024-01-10 13:39:18 +0100

    - Activities refactorized - Fixed text with dp

--- app/src/main/java/com/example/gamequiz/Main.java
+++ app/src/main/java/com/example/gamequiz/Main.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Register extends AppCompatActivity {
+public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_main);
     }
 }
--- app/src/main/java/com/example/gamequiz/Splash.java
+++ app/src/main/java/com/example/gamequiz/Splash.java
@@ -33,7 +33,7 @@ public class Splash extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Splash.this, Login.class);
+                Intent intent = new Intent(Splash.this, Main.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
