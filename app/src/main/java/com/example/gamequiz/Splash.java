9a3b9bec048df5837c1ae322e90a411212dbbafe
martinguijarro <martin.gg222@gmail.com>
2024-01-10 13:16:56 +0100

    Splash is now loaded with Glide

--- app/src/main/java/com/example/gamequiz/Splash.java
+++ app/src/main/java/com/example/gamequiz/Splash.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 public class Splash extends AppCompatActivity {
 
@@ -12,9 +16,16 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ImageView splashImage = findViewById(R.id.splashgif);
+
+        Glide.with(this)
+                .load(R.drawable.logo)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(splashImage);
+
         // LLAMADA AL MÉTODO
         openApp();
-        
     }
 
     private void openApp(){
