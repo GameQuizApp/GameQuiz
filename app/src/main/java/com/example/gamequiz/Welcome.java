856a1c750dbebe0101ecd27d2c46f7c98bd7022d
Alejandro <alejandrord3006@gmail.com>
2024-01-12 12:44:01 +0100

    Connect welcome with register and main

--- app/src/main/java/com/example/gamequiz/Welcome.java
+++ app/src/main/java/com/example/gamequiz/Welcome.java
@@ -2,7 +2,9 @@ package com.example.gamequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Welcome extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class Welcome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
     }
+
+    public void openLogin(View v) {
+        Intent intent = new Intent(Welcome.this, Login.class);
+        startActivity(intent);
+    }
+
+    public void openRegister(View v) {
+        Intent intent = new Intent(Welcome.this, Register.class);
+        startActivity(intent);
+    }
+
 }
