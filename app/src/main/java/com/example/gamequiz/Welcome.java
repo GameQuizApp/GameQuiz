2c506d1afbd1fa38df9e1d341897bd08fe875ad2
martinguijarro <martin.gg222@gmail.com>
2024-01-15 12:30:26 +0100

    - Activities refactorized - Fixed text with dp

--- app/src/main/java/com/example/gamequiz/Welcome.java
+++ app/src/main/java/com/example/gamequiz/Welcome.java
@@ -1,14 +1,33 @@
 package com.example.gamequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Welcome extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        CardView registerCardView = findViewById(R.id.registerCV);
+        CardView loginCardView = findViewById(R.id.loginCV);
+
+        registerCardView.setVisibility(View.INVISIBLE);
+        loginCardView.setVisibility(View.INVISIBLE);
+    }
+
+    public void openRegister() {
+        CardView registerCardView = findViewById(R.id.registerCV);
+
+        registerCardView.setVisibility(View.VISIBLE);
+    }
+
+    public void openLogin() {
+        CardView loginCardView = findViewById(R.id.loginCV);
+
+        loginCardView.setVisibility(View.VISIBLE);
     }
 }
