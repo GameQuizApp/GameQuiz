db9f4b6caaa161def826e4179fb43e02e836fd84
martinguijarro <martin.gg222@gmail.com>
2024-01-24 13:59:03 +0100

    - Added Bottomsheet

--- app/src/main/java/com/example/gamequiz/Main.java
+++ app/src/main/java/com/example/gamequiz/Main.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class Main extends AppCompatActivity {
 
     Button loginbutton;
     Button signupbutton;
+    Button resultbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +33,22 @@ public class Main extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Main.this, Signup.class));
+            }
+        });
+
+        resultbutton = findViewById(R.id.result);
+        resultbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                BottomSheetDialog dialog = new BottomSheetDialog(Main.this);
+
+                View vista = LayoutInflater.from(getApplicationContext()).inflate(R.layout.result_bottomsheet, null);
+
+                dialog.setCancelable(true);
+                dialog.setContentView(vista);
+
+                dialog.show();
             }
         });
     }
