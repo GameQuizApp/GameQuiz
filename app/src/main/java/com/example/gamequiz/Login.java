e8f853676ad2621fef02b4b48e70ae4d205b0a97
Kevin <Kevbast936@gmail.com>
2024-02-01 12:25:46 +0100

    activity_login.xml modificado+ openRegister

--- app/src/main/java/com/example/gamequiz/Login.java
+++ app/src/main/java/com/example/gamequiz/Login.java
@@ -2,7 +2,9 @@ package com.example.gamequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void openRegister(View v) {
+        Intent intent= new Intent(Login.this, Signup.class);
+        startActivity(intent);
     }
 }
