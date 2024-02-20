c106600e1d1c6da71332da1da651ee82619573e8
Alejandro <alejandrord3006@gmail.com>
2024-02-20 13:53:57 +0100

    - Update Classic Mode design

--- app/src/main/java/com/kevmartal/gamequiz/FragPlay.java
+++ app/src/main/java/com/kevmartal/gamequiz/FragPlay.java
@@ -1,5 +1,6 @@
 package com.kevmartal.gamequiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +25,7 @@ public class FragPlay extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    Button btnPlay;
 
     public FragPlay() {
         // Required empty public constructor
@@ -58,7 +61,17 @@ public class FragPlay extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflar el diseño del fragment
-        return inflater.inflate(R.layout.fragment_play, container, false);
+        // Inflar el diseño del fragmento
+        View view = inflater.inflate(R.layout.fragment_play, container, false);
+
+        btnPlay = view.findViewById(R.id.buttonPlay);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), GameActivity.class));
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/com/kevmartal/gamequiz/GameActivity.java
+++ app/src/main/java/com/kevmartal/gamequiz/GameActivity.java
@@ -3,6 +3,7 @@ package com.kevmartal.gamequiz;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +14,7 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
@@ -86,6 +88,9 @@ public class GameActivity extends AppCompatActivity {
         ImageView imageView = findViewById(R.id.imageView);
         Glide.with(this)
                 .load(imageUrl)
+                .centerCrop() // Centrar la imagen y recortar los bordes
+                .diskCacheStrategy(DiskCacheStrategy.ALL) // Almacenar la imagen en caché para que tarde menos en ser cargada a partir del primer intento
+                .placeholder((new ColorDrawable(this.getResources().getColor(R.color.morado)))) // Añadir un color donde vaya la imagen para manejar lo que pasa en ese espacio hasta que la imagen es cargada
                 .error(R.drawable.ic_launcher) // R.drawable.error_image es la imagen que se mostrará en caso de error
                 .listener(new RequestListener<Drawable>() {
                     @Override
