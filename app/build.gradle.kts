8c2592fd1245fa9f63031411fa18f2c7707c158b
Alejandro <alejandrord3006@gmail.com>
2024-02-25 22:48:56 +0100

    - New version 0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.kevmartal.gamequiz"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.5"
+        versionCode = 7
+        versionName = "0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
